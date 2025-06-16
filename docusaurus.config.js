d206a822743586222171dddf5cc7f0dff40ae9cf
Xiang Zhang <melonq@foxmail.com>
2025-06-16 21:06:02 +0800

    Modify projectName

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,12 +25,12 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/oligo-official-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'oligo-com-au', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'oligo-official-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
