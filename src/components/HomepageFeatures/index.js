848768a292014f768f64ab017a0a900a34c79e1e
Xiang Zhang <melonq@foxmail.com>
2025-06-16 23:31:52 +0800

    Add link on homepage

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,22 +4,22 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Developer Guides',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        used to get your website up and running quickly. <a href='developer'>Click me to read more.</a>
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'PayTo API',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        ahead and move your docs into the <code>docs</code> directory. <a href='api'>Click me to read more.</a>
       </>
     ),
   },
